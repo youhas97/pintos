287d2da95393ca7b25425c5eb8bb2afe7e940bd8
Yousef Hashem <youhas97@gmail.com>
2018-07-08 20:53:50 +0200

    added arg paranoia in syscall and f->eax = in syscall handler

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -11,88 +11,6 @@
 
 static void syscall_handler (struct intr_frame *);
 
-void
-syscall_init (void)
-{
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
-}
-
-void halt(void){
-    power_off();
-}
-
-bool create (const char *file, unsigned initial_size){
-    return filesys_create(file, initial_size);
-}
-
-int open (const char *file){
-    struct thread *t = thread_current();
-    struct file *file_ptr = filesys_open(file);
-
-    if (file_ptr) {
-        int i;
-        for (i = 0; i<MAX_FILES; ++i) {
-            if (t->files[i] == NULL) {
-                t->files[i] = file_ptr;
-                return i+OFFSET;
-            }
-        }
-    }
-    file_close(file_ptr);
-    return -1;
-}
-
-void close(int fd) {
-    struct thread *t = thread_current();
-
-    if (fd >= OFFSET && fd < MAX_FILES + OFFSET) {
-        struct file *file_ptr = t->files[fd-OFFSET];
-
-        if (file_ptr) {
-            file_close(file_ptr);
-            t->files[fd-OFFSET] = NULL;
-        }
-    }
-}
-
-int read (int fd, void *buffer, unsigned size){
-    if (fd == 0) {
-        int bytes_read;
-        for (bytes_read = 0;  bytes_read < size; ++bytes_read) {
-            char c = input_getc();
-            *(char*)buffer = c;
-            (char*)buffer++;
-        }
-        return bytes_read;
-    }
-
-    struct thread *t = thread_current();
-    if (fd >= OFFSET && fd < MAX_FILES + OFFSET) {
-        struct file *file_ptr = t->files[fd-OFFSET];
-
-        if (file_ptr)
-            return file_read(file_ptr, buffer, size);
-    }
-    return -1;
-}
-
-int write (int fd, const void *buffer, unsigned size){
-    if (fd == 1) {
-        putbuf((char*)buffer, size);
-        return size;
-    }
-
-    struct thread *t = thread_current();
-    if (fd >= OFFSET && fd < MAX_FILES + OFFSET) {
-        struct file *file_ptr = t->files[fd-OFFSET];
-
-        if (file_ptr)
-            return file_write(file_ptr, buffer, size);
-    }
-    return -1;
-
-}
-
 static bool
 is_valid_ptr(const void *p) {
   struct thread *t = thread_current();
@@ -127,6 +45,101 @@ is_valid_buf(const void *buf, size_t size) {
   return true;
 }
 
+void
+syscall_init (void)
+{
+  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+void halt(void){
+    power_off();
+}
+
+bool create (const char *file, unsigned initial_size){
+    if(is_valid_ptr(file) && is_valid_str(*file))
+        return filesys_create(file, initial_size);
+    exit(-1);
+    return false;
+}
+
+int open (const char *file){
+    if (!is_valid_ptr(file) || !is_valid_str(file))
+        exit(-1);
+    else {
+        struct thread *t = thread_current();
+        struct file *file_ptr = filesys_open(file);
+        if (file_ptr) {
+            int i;
+            for (i = 0; i<MAX_FILES; ++i) {
+                if (t->files[i] == NULL) {
+                    t->files[i] = file_ptr;
+                    return i+OFFSET;
+                }
+            }
+        }
+        file_close(file_ptr);
+    }
+    return -1;
+}
+
+void close(int fd) {
+    struct thread *t = thread_current();
+
+    if (fd >= OFFSET && fd < MAX_FILES + OFFSET) {
+        struct file *file_ptr = t->files[fd-OFFSET];
+
+        if (file_ptr) {
+            file_close(file_ptr);
+            t->files[fd-OFFSET] = NULL;
+        }
+    }
+}
+
+int read (int fd, void *buffer, unsigned size){
+    if(!is_valid_buf(buffer, size) || !is_valid_ptr(buffer))
+        exit(-1);
+    else {
+        if (fd == 0) {
+            int bytes_read;
+            for (bytes_read = 0;  bytes_read < size; ++bytes_read) {
+                char c = input_getc();
+                *(char*)buffer = c;
+                (char*)buffer++;
+            }
+            return bytes_read;
+        }
+
+        struct thread *t = thread_current();
+        if (fd >= OFFSET && fd < MAX_FILES + OFFSET) {
+            struct file *file_ptr = t->files[fd-OFFSET];
+
+            if (file_ptr)
+                return file_read(file_ptr, buffer, size);
+        }
+    }
+    return -1;
+}
+
+int write (int fd, const void *buffer, unsigned size){
+    if (!is_valid_buf(buffer, size) || !is_valid_ptr(buffer))
+        exit(-1);
+    else {
+        if (fd == 1) {
+            putbuf((char*)buffer, size);
+            return size;
+        }
+
+        struct thread *t = thread_current();
+        if (fd >= OFFSET && fd < MAX_FILES + OFFSET) {
+            struct file *file_ptr = t->files[fd-OFFSET];
+
+            if (file_ptr)
+                return file_write(file_ptr, buffer, size);
+        }
+    }
+    return -1;
+}
+
 tid_t exec (const char *cmd_line) {
     if (is_valid_ptr(cmd_line) && is_valid_str(cmd_line))
         return process_execute(cmd_line);
@@ -149,36 +162,52 @@ int wait (tid_t pid) {
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
-    int *arg = (int*)f->esp;
+    if(is_valid_ptr(f->esp)) {
+        int *arg = (int*)f->esp;
 
-    switch(arg[0]){
-        case SYS_HALT:
-            halt();
-            break;
-        case SYS_EXIT:
-            exit((int)arg[1]);
-            break;
-        case SYS_CREATE:
-            f->eax = create((char*)arg[1], (unsigned)arg[2]);
-            break;
-        case SYS_OPEN:
-            f->eax = open((char*)arg[1]);
-            break;
-        case SYS_READ:
-            f->eax = read((int)arg[1], (void*)arg[2], (unsigned)arg[3]);
-            break;
-        case SYS_WRITE:
-            f->eax = write((int)arg[1], (void*)arg[2], (unsigned)arg[3]);
-            break;
-        case SYS_CLOSE:
-            close((int)arg[1]);
-            break;
-        case SYS_EXEC:
-            exec((char*)arg[1]);
-            break;
-        case SYS_WAIT:
-            wait((tid_t)arg[1]);
-        default:
-            printf ("system call! down here\n");
+        switch(arg[0]){
+            case SYS_HALT:
+                halt();
+                break;
+            case SYS_EXIT:
+                if(is_valid_ptr(&arg[1]))
+                    exit((int)arg[1]);
+                else
+                    exit(-1);
+                break;
+            case SYS_CREATE:
+                f->eax = create((char*)arg[1], (unsigned)arg[2]);
+                break;
+            case SYS_OPEN:
+                if(is_valid_ptr(&arg[1]))
+                    f->eax = open((char*)arg[1]);
+                else
+                    exit(-1);
+                break;
+            case SYS_READ:
+                f->eax = read((int)arg[1], (void*)arg[2], (unsigned)arg[3]);
+                break;
+            case SYS_WRITE:
+                f->eax = write((int)arg[1], (void*)arg[2], (unsigned)arg[3]);
+                break;
+            case SYS_CLOSE:
+                if(is_valid_ptr(&arg[1]))
+                    close((int)arg[1]);
+                else
+                    exit(-1);
+                break;
+            case SYS_EXEC:
+                f->eax = exec((char*)arg[1]);
+                break;
+            case SYS_WAIT:
+                if(is_valid_ptr(&arg[1]))
+                    f->eax = wait((tid_t)arg[1]);
+                else
+                    exit(-1);
+            default:
+                printf ("system call! down here\n");
+        }
     }
+    else
+        exit(-1);
 }
