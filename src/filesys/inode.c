97a2494cd17b39b58b15694ad2808f74c0366a93
Yousef Hashem <youhas97@gmail.com>
2018-07-12 17:00:30 +0200

    change back read/write synch implementation to use deny_write_count

--- src/filesys/inode.c
+++ src/filesys/inode.c
@@ -39,8 +39,6 @@ struct inode
     int deny_write_cnt;                 /* 0: writes ok, >0: deny writes. */
     struct inode_disk data;             /* Inode content. */
 
-    int readers;
-    struct lock rmutex;                /* lock for changing value of readers */
     struct semaphore write_sema;       /* lock for reading/writing */
     struct lock dwc_lock;              /* deny_write_cnt lock */
     struct lock oc_lock;               /* lock for opening/closing */
@@ -150,8 +148,6 @@ inode_open (disk_sector_t sector)
   sema_init(&inode->write_sema, 1);
   lock_init(&inode->dwc_lock);
   lock_init(&inode->open_cnt_lock);
-  lock_init(&inode->rmutex);
-  inode->readers = 0;
 
   inode->sector = sector;
   inode->open_cnt = 1;
@@ -234,10 +230,11 @@ off_t
 inode_read_at (struct inode *inode, void *buffer_, off_t size, off_t offset)
 {
   /* prevents simultaneous change of readers */
-  lock_acquire(&inode->rmutex);
-  if(++(inode->readers) == 1)
+  lock_acquire(&inode->dwc_lock);
+  inode_deny_write(inode);
+  if(inode->deny_write_cnt == 1)
     sema_down(&inode->write_sema);
-  lock_release(&inode->rmutex);
+  lock_release(&inode->dwc_lock);
 
   uint8_t *buffer = buffer_;
   off_t bytes_read = 0;
@@ -286,10 +283,11 @@ inode_read_at (struct inode *inode, void *buffer_, off_t size, off_t offset)
   free (bounce);
 
   /* prevents simultaneous change of deny_write_cnt */
-  lock_acquire(&inode->rmutex);
-  if (--(inode->readers) == 0)
+  lock_acquire(&inode->dwc_lock);
+  inode_allow_write(inode);
+  if(inode->deny_write_cnt == 0)
     sema_up(&inode->write_sema);
-  lock_release(&inode->rmutex);
+  lock_release(&inode->dwc_lock);
 
   return bytes_read;
 }
